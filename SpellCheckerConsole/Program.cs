b53d444066dd08ecaee4e001934217f1080aca7d
taylorstapleton <taylorstapleton@gmail.com>
2014-03-06 17:20:14 -0700

    first setup commit

---
+++ SpellCheckerConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellCheckerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string hello = Console.ReadLine();
+                print(hello);
+            }
+        }
+
+        static void print(string toPrint)
+        {
+
+            Console.WriteLine(toPrint);
+        }
+    }
+}
